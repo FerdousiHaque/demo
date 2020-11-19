6e31417c67e45047c9850557ede4461d31f56cc6
Ferdousi Haque <ferdousi160228@gmail.com>
2020-11-19 14:46:14 +0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,11 +3,16 @@ var rootRef = firebase.database().ref().child("products");
 rootRef.on("child_added", snap => {
     //alert(snap.val());
     var desp = snap.child("description").val();
-    var image = document.createElement("img");
-    var imageParent = document.getElementById("product_section");
-    image.className = "col-md-6 col-sm-6";
-    image.src = snap.child("image").val();
-    imageParent.appendChild(image);
+    var image = snap.child("image").val();
+    var image_and_desp_string = 
+   '<div class="row">'
+     + '<div class="col-md-6 col-sm-6 productDetails">'
+         + '<p>' + desp + '</p>'
+     + '</div>'
+     + '<div class="col-md-6 col-sm-6">'
+         + '<img src="' + image + '">'
+     + '</div>'
+    + '</div>';
 
-    $("#product_section").append('<div class="row"><div class="col-md-6 col-sm-6 productDetails"><p>'+desp+'</p></div></div>');
+    $("#product_section").append(image_and_desp_string);
 });
