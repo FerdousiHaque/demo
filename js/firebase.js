85eaf8184faf21a6d94831ff03d52b34b7970449
Ferdousi Haque <ferdousi160228@gmail.com>
2020-11-19 20:51:41 +0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,34 +1,20 @@
-// Loop through users in order with the forEach() method. The callback
-// provided to forEach() will be called synchronously with a DataSnapshot
-// for each child:
-var query = firebase.database().ref("products").orderByKey();
-query.once("value")
-  .then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      const key = Number(childSnapshot.key);
-      console.log(key);
-      // childData will be the actual contents of the child
-      var desp = childSnapshot.child("description").val();
-      var img = childSnapshot.child("image").val();
+var rootRef = firebase.database().ref().child("products");
+rootRef.on("child_added", snap => { //get all data from Firebase
+    var key = snap.key;
+    var isOdd = parseInt(snap.key) % 2 == 1;
+    var desp = snap.child("description").val();
+    var image = snap.child("image").val();
+    var imageString = '<div class="col-md-6 col-sm-6">'
+         + '<img src="' + image + '">'
+     + '</div>';
+    var despString = '<div class="col-md-6 col-sm-6 productDetails">'
+         + '<p>' + desp + '</p>'
+     + '</div>';
+    var image_and_desp_string =       
+   '<div class="row">'
+     + (isOdd ? despString : imageString)  //show value from Firebase, image then description
+     + (isOdd ? imageString : despString)   //show value from Firebase, description then image
+    + '</div>';
 
-      key % 2 === 0 ?
-      $("#product_section").append(          //show value from Firebase, image then description
-        '<div class="row">'
-          + '<div class="col-md-6 col-sm-6">'
-              + '<img src="' + img + '">'
-          + '</div>'
-          + '<div class="col-md-6 col-sm-6 productDetails">'
-              + '<p>' + desp + '</p>'
-          + '</div>'
-         + '</div>')
-      : $("#product_section").append(        //show value from Firebase, description then image
-        '<div class="row">'
-          + '<div class="col-md-6 col-sm-6 productDetails">'
-              + '<p>' + desp + '</p>'
-          + '</div>'
-          + '<div class="col-md-6 col-sm-6">'
-              + '<img src="' + img + '">'
-          + '</div>'
-         + '</div>');
-  });
+    $("#product_section").append(image_and_desp_string);
 });
