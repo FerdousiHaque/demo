5f6d6cfbf8ac3fb147a02cdb8bed2df1b21ce9da
Ferdousi Haque <ferdousi160228@gmail.com>
2020-11-19 20:15:16 +0600

    Updated to change product showing order

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,35 @@
+// Loop through users in order with the forEach() method. The callback
+// provided to forEach() will be called synchronously with a DataSnapshot
+// for each child:
+var query = firebase.database().ref("products").orderByKey();
+query.once("value")
+  .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      const key = Number(childSnapshot.key);
+      console.log(key);
+      // childData will be the actual contents of the child
+      var desp = childSnapshot.child("description").val();
+      var img = childSnapshot.child("image").val();
+      var image_and_desp_string;
 
-var rootRef = firebase.database().ref().child("products");
-rootRef.on("child_added", snap => {
-    //alert(snap.val());
-    var desp = snap.child("description").val();
-    var image = snap.child("image").val();
-    var image_and_desp_string = 
-   '<div class="row">'
-     + '<div class="col-md-6 col-sm-6 productDetails">'
-         + '<p>' + desp + '</p>'
-     + '</div>'
-     + '<div class="col-md-6 col-sm-6">'
-         + '<img src="' + image + '">'
-     + '</div>'
-    + '</div>';
-
-    $("#product_section").append(image_and_desp_string);
+      key % 2 === 0 ?
+      $("#product_section").append(          //get value from Firebase and show accordingly
+        '<div class="row">'
+          + '<div class="col-md-6 col-sm-6">'
+              + '<img src="' + img + '">'
+          + '</div>'
+          + '<div class="col-md-6 col-sm-6 productDetails">'
+              + '<p>' + desp + '</p>'
+          + '</div>'
+         + '</div>')
+      : $("#product_section").append(            //get value from Firebase and show accordingly
+        '<div class="row">'
+          + '<div class="col-md-6 col-sm-6 productDetails">'
+              + '<p>' + desp + '</p>'
+          + '</div>'
+          + '<div class="col-md-6 col-sm-6">'
+              + '<img src="' + img + '">'
+          + '</div>'
+         + '</div>');
+  });
 });
